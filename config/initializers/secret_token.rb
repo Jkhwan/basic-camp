42ead5e615e7d9b293c222e11355b748424c3d1d
Jason Wan <jwan823@gmail.com>
2013-11-21 04:26:58 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BasicCamp::Application.config.secret_key_base = 'daa4ba0a6c89f507e2705238353d7ec7ceb99280727c0008c5f57aa5fe5afd719d3362367060fa0cf4761d9009b38f8bdb75dc3a0d9ee05b60b9aed3df8e5672'
