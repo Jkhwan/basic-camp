dfb9a1d59e7ad3f00319a1418bee029e3a572b54
Jason Wan <jwan823@gmail.com>
2013-11-29 23:38:00 +0000

    Fixed bug regarding default profile pic

--- app/helpers/assets_helper.rb
+++ app/helpers/assets_helper.rb
@@ -6,4 +6,20 @@ module AssetsHelper
       name.length > 10 ? name[0..10] + '..' : name
     end
   end
+
+  def user_image_thumb_url(user)
+    if @user.avatar.blank?
+      "/assets/default_photo_thumb.jpg"
+    else
+      @user.avatar.url(:small_thumb)
+    end
+  end
+
+  def user_image_url(user)
+    if @user.avatar.blank?
+      "/assets/default_photo.jpg"
+    else
+      @user.avatar.url(:thumb)
+    end
+  end
 end
